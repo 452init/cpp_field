6216891fee8e61aefa37c2eae550f9817f67c4f3
452init <clinsmol10@gmail.com>
2026-03-31 20:38:56 +0300

    Log levels

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -2,14 +2,23 @@
 
 namespace log_line {
 std::string message(std::string line) {
-    // return the message
+    int startIndex = line.find(": ");
+    std::string logMessage = line.substr(startIndex + 2);
+    return logMessage;
 }
 
 std::string log_level(std::string line) {
-    // return the log level
+    int endIndex = line.find("]");
+    std::string logLevel = line.substr(1, endIndex - 1);
+    return logLevel;
 }
 
 std::string reformat(std::string line) {
-    // return the reformatted message
+    int startIndx = line.find(": ");
+    std::string myLogMessage = line.substr(startIndx + 2);
+
+    int character = line.find("]");
+    std::string myLogLevel = line.substr(1, character - 1);
+    return myLogMessage + " (" + myLogLevel + ")";
 }
-}  // namespace log_line
+}
