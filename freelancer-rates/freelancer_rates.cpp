41a11ab5863344c6808c9da078c86a5bd91638de
452init <clinsmol10@gmail.com>
2026-05-28 06:32:47 +0300

    code refactor

--- freelancer-rates/freelancer_rates.cpp
+++ freelancer-rates/freelancer_rates.cpp
@@ -3,31 +3,29 @@
 
 // daily_rate calculates the daily rate given an hourly rate
 double daily_rate(double hourly_rate) {
-		constexp int billable_work_day_hours = 8;
+		constexpr int billable_work_day_hours = 8;
   	return hourly_rate * billable_work_day_hours;
 }
 
 // apply_discount calculates the price after a discount
 double apply_discount(double before_discount, double discount) {
-    return before_discount - (before_discount * (discount/100));
+		constexpr int percentage = 100;
+    return before_discount - (before_discount * (discount/percentage));
 }
 
 // monthly_rate calculates the monthly rate, given an hourly rate and a discount
 // The returned monthly rate is rounded up to the nearest integer.
 int monthly_rate(double hourly_rate, double discount) {
-	double val = (hourly_rate - (hourly_rate * (discount/100.0)))*176;
-	int round = (int)val;
+	constexpr int work_days_in_month = 22;
 
-	if (val > round){
-		round++;
-	}
-	return round;
+	const double discounted_daily rate =
+	apply_discount(daily_rate(hourly_rate), discount);
+	return ceil(work_days_in_month * discounted_daily_rate);
 }
 
 // days_in_budget calculates the number of workdays given a budget, hourly rate,
 // and discount The returned number of days is rounded down (take the floor) to
 // the next integer.
 int days_in_budget(int budget, double hourly_rate, double discount) {
-	int result = (int)(budget/(hourly_rate - (hourly_rate * (discount/100))))/8;
-	return result;
+	return (int)(budget/(apply_discount(daily_rate(hourly_rate), discount)));
 }
