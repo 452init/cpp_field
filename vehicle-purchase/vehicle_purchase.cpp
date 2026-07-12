cd9b0864d04d6059940b3d57c157ca4183134d31
452init <clinsmol10@gmail.com>
2026-07-12 16:38:09 +0300

    optimization

--- vehicle-purchase/vehicle_purchase.cpp
+++ vehicle-purchase/vehicle_purchase.cpp
@@ -5,29 +5,26 @@ namespace vehicle_purchase {
 // needs_license determines whether a license is needed to drive a type of
 // vehicle. Only "car" and "truck" require a license.
 bool needs_license(std::string kind) {
-    if (kind == "car" || "truck"){
-        return true;
-    }else{
-        return false;  
-    }
+    return (kind == "car" || kind == "truck");
 }
 
 // choose_vehicle recommends a vehicle for selection. It always recommends the
 // vehicle that comes first in lexicographical order.
 std::string choose_vehicle(std::string option1, std::string option2) {
-    int result = option1.compare(option2);
+	std::string statement = " is clearly the better choice.";
     
-    if (result < 0){
-        return option1 + "is clearly the better choice";
-    }else{
-        return option2 + "is clearly the better choice";
-    }
+    if (option1 < option2){
+			return option1 + statement;
+		}else{
+			return option2 + statement;
+		}
 }
 
 // calculate_resell_price calculates how much a vehicle can resell for at a
 // certain age.
 double calculate_resell_price(double original_price, double age) {
-    double finalPrice;
+    double finalPrice = 0;
+
     if (age < 3){
         finalPrice = original_price * (1 - 0.2);
     }else if(age >= 3 && age < 10){
